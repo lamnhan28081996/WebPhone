b6551454892576cf056b63d84ff69a1b716bdcb7
lamnhan28081996 <lamnhan28081996@gmail.com>
2018-06-12 17:19:42 +0700

    Làm tới phần ajax và jquery

---
+++ depot/app/controllers/store_controller.rb
@@ -0,0 +1,8 @@
+class StoreController < ApplicationController
+	# include CurrentCart
+	# before_action :set_cart
+  def index
+  	@products = Product.all.paginate(:page => params[:page], :per_page => 5)
+  	@cart = current_cart
+  end
+end
--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ApplicationRecord
+    validates :title, :description, :image_url, :presence => true
+    validates :price, :numericality => {:greater_than_or_equal_to => 1.0}
+    validates :title, :uniqueness => true
+    validates :image_url, :format => {
+        :with => %r{\.(gif|jpg|png)\Z}i,
+        :message => 'Chi nhan file GIF, JPG, PNG'
+    }
+    has_many :line_items
+    before_destroy :check_if_has_line_item
+  
+	private
+  
+    def check_if_has_line_item
+        if line_items.empty?
+            return true
+        else
+            errors.add(:base, 'This product has a LineItem')
+            return false
+        end
+    end
 end
